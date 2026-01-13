6ca9caa3d761e48ef075e51273cce4f069877142
jiang <111@qq.com>
2026-01-13 09:56:34 +0800

    更新脚本

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,16 +4,10 @@ module.exports = {
       name: "blog",
       script: "node_modules/next/dist/bin/next",
       args: "start",
-      instances: "max", // 开启集群模式，根据 CPU 核心数自动分配
-      exec_mode: "cluster",
       autorestart: true,
       watch: false,
       max_memory_restart: "1G",
       env: {
-        PORT: 3000,
-        NODE_ENV: "production",
-      },
-      env_production: {
         PORT: 3000,
         NODE_ENV: "production",
       },
